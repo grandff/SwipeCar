2b72584dbad5786743abd227b9641bea5585de54
김정민 <dsaqaz421@naver.com>
2020-03-20 23:37:09 +0900

    Initial commit

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    float speed = 0;
+    Vector2 startPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 스와이프 길이를 구해줌
+        if(Input.GetMouseButtonDown(0)){
+            // 마우스를 클릭한 좌표
+            this.startPos = Input.mousePosition;
+        }else if(Input.GetMouseButtonUp(0)){
+            // 마우스 버튼에서 손가락을 떼었을 때
+            Vector2 endPos = Input.mousePosition;
+            float swipeLength = (endPos.x - this.startPos.x);
+
+            // 스와이프 길이를 처음 속도로 변경함
+            this.speed = swipeLength / 1000.0f;
+
+            // 효과음 재생
+            GetComponent<AudioSource>().Play();
+        }
+
+        transform.Translate(this.speed, 0, 0);
+        this.speed *= 0.98f;
+        /*      초기 소스
+        
+        if(Input.GetMouseButtonDown(0)){        // 마우스를 클릭하면
+            this.speed = 0.2f;                  // 처음 속도 설정
+
+            
+            //    Translate는 현재 지점에서 해당 방향으로 수치만큼 이동하는 거임. 해당 좌표로 이동하는게 아님.
+            
+            transform.Translate(this.speed, 0, 0);  
+
+            this.speed *= 0.98f;        // 속도 감소
+        }
+        */
+    }
+}
---
+++ Assets/Scripts/GameDirector.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;       // UI를 사용하는데 필요함
+
+public class GameDirector : MonoBehaviour
+{
+    GameObject car;
+    GameObject flag;
+    GameObject distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*
+            Find : 오브젝트 명을 인수로 전달하고 인수명이 게임 씬 안에 있으면 해당 오브젝트 반환
+        */
+        this.car = GameObject.Find("car");
+        this.flag = GameObject.Find("flag");
+        this.distance = GameObject.Find("Distance");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float length = this.flag.transform.position.x - this.car.transform.position.x;          // 게임 오브젝트의 좌푤르 구해서 거리를 계산
+
+        // 깃발을 지나친 경우 게임 오버 표시
+        if (length >= 0){
+            this.distance.GetComponent<Text>().text = "목표 지점까지 " + length.ToString("F2") + "m";
+        }else{
+            this.distance.GetComponent<Text>().text = "게임 오바";
+        }
+
+        /*
+            자신 이외의 오브젝트 컴포넌트에 접근하는 방법
+            1) Find 메서드로 오브젝트를 찾음
+            2) GetComponent 메서드로 오브젝트의 컴포넌트를 구함
+            3) 컴포넌트를 가진 데이터에 접근
+        */
+        
+    }
+}
